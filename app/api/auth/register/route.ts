ef56a9ca567a6f8b65b2bb1348cee875c6a4a2b7
shreyas367 <shreyas367@users.noreply.github.com>
2025-08-13 19:22:41 +0530

    singnup

---
+++ app/api/auth/register/route.ts
@@ -0,0 +1,45 @@
+import { connectToDatabase } from "@/lib/db";
+import User from "@/models/User";
+import { error } from "console";
+import { NextRequest,NextResponse } from "next/server";
+
+export async function POST(request:NextRequest){
+    try{
+        const{email,password}=await request.json();
+
+        if(!email|| !password){
+            return NextResponse.json(
+                {error:"email and password are required for register"}
+                ,{status:400}
+            
+            )
+         }
+
+    await connectToDatabase();
+
+    const existingUser=await User.findOne({email})
+    if(existingUser){
+        return NextResponse.json(
+            {error:"user already exist in registered db"},
+            {status:400}
+
+        );
+
+    }
+    await User.create({
+        email,password
+    })
+
+    return NextResponse.json(
+        {
+        message:"new user aaayaaa hai registeration done bro"
+    }
+    ,
+    {status:200}
+)
+}
+catch{
+
+}
+
+}
